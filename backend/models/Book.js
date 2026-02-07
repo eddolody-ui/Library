2bfe2ccf0a22e30672d5787c35e428dc3315ae76
Luke <eddolody@gmail.com>
2026-02-07 22:01:32 +0630

    c3

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
   bookId: {
     type: String,
-    required: true,
+    required: false,
     unique: true,
   },
   title: {
