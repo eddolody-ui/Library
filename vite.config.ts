e25ad8f7b948e6cf7537383ded5b3883b589dbcd
eddolody-ui <eddolody@gmail.com>
2026-02-07 15:23:40 +0630

    c5

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+      build: {
+    outDir: "dist",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -18,5 +21,5 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  }
+  },
 })
